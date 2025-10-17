75aec3a0150857c73d304fe6a00d80e964cad689
Michael <noedigsti@gmail.com>
2025-10-17 11:32:26 +1000

    update

--- example_client.cc
+++ example_client.cc
@@ -2,18 +2,16 @@
 #include <memory>
 #include <string>
 
-#include <grpcpp/grpcpp.h>
 #include "example.grpc.pb.h"
 #include "example.pb.h"
+#include <grpcpp/grpcpp.h>
 
-class ProcessingClient
-{
+class ProcessingClient {
 public:
   ProcessingClient(std::shared_ptr<grpc::Channel> channel)
       : stub_(ProcessingServices::NewStub(channel)) {}
 
-  double ComputeSum(double x, double y, double z)
-  {
+  double ComputeSum(double x, double y, double z) {
     Point3 request;
     request.set_x(x);
     request.set_y(y);
@@ -24,13 +22,11 @@ public:
 
     grpc::Status status = stub_->computeSum(&context, request, &response);
 
-    if (status.ok())
-    {
+    if (status.ok()) {
       return response.value();
-    }
-    else
-    {
-      std::cout << "RPC failed: " << status.error_code() << ": " << status.error_message() << std::endl;
+    } else {
+      std::cout << "RPC failed: " << status.error_code() << ": "
+                << status.error_message() << std::endl;
       return 0;
     }
   }
@@ -39,16 +35,17 @@ private:
   std::unique_ptr<ProcessingServices::Stub> stub_;
 };
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   // Create a client
-  ProcessingClient client(grpc::CreateChannel("localhost:9999", grpc::InsecureChannelCredentials()));
+  ProcessingClient client(grpc::CreateChannel(
+      "localhost:9999", grpc::InsecureChannelCredentials()));
 
   // Test the ComputeSum RPC
   double x = 1.0, y = 2.0, z = 3.0;
   double result = client.ComputeSum(x, y, z);
 
-  std::cout << "Sum of " << x << ", " << y << ", and " << z << " is: " << result << std::endl;
+  std::cout << "Sum of " << x << ", " << y << ", and " << z << " is: " << result
+            << std::endl;
 
   return 0;
 }
--- example_server.cc
+++ example_server.cc
@@ -1,19 +1,17 @@
-#include <grpcpp/grpcpp.h>
 #include "example.grpc.pb.h"
 #include "example.pb.h"
+#include <grpcpp/grpcpp.h>
 
-class ProssessingImpl : public ProcessingServices::Service
-{
-  ::grpc::Status computeSum(::grpc::ServerContext *context, const ::Point3 *request, ::Numeric *response)
-  {
+class ProssessingImpl : public ProcessingServices::Service {
+  ::grpc::Status computeSum(::grpc::ServerContext *context,
+                            const ::Point3 *request, ::Numeric *response) {
     std::cout << "Hey" << std::endl;
     response->set_value(request->x() + request->y() + request->z());
     return grpc::Status::OK;
   }
 };
 
-int main()
-{
+int main() {
   ProssessingImpl service;
   grpc::ServerBuilder builder;
   builder.AddListeningPort("0.0.0.0:9999", grpc::InsecureServerCredentials());
